59c1b98284f16eab5cae5cdcc3215b1b4bc9ec4b
Kenzie Campbell <kenzie@route19.com>
2011-09-12 20:55:07 -0300

    Initial setup and import

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+gem 'jquery-rails'
+
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+group :development, :test do
+	gem 'sqlite3'
+	gem 'cucumber-rails'
+	gem 'database_cleaner'
+  # gem 'rspec-rails'
+end
+
+group :test do
+  gem 'turn', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paystub::Application.config.secret_token = '4a099ade55815f6b5ab87373318f82d975e2840711052119ef7258ae0ad9dad87f8640f331a03afa44e901af1e3ad2791eaaff048ff219ca8a3d111e747793cf'
